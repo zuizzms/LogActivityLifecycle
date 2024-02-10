223e686ebe58dbeeddb324980ea3fedf7e119e08
Zuizz Saeed <zuizzms@bu.edu>
2024-02-10 17:27:24 -0500

    more lifecycle functions

--- app/src/main/java/com/example/logactivitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/logactivitylifecycle/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.logactivitylifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate(Bundle?) called")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
